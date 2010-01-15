58ef13fef58010b2bc6fbca1c44c8517eafb02a4
hdolben <hdolben@67ca1e1c-01e3-11df-9f77-c71dee008b03>
2010-01-15 15:23:10 +0000

    initial import

---
+++ collidoscope/trunk/org/dolben/poly/Cube.java
@@ -0,0 +1,12 @@
+/**
+ *  Copyright (c) 2005-2006 by Hank Dolben
+ *  Licensed under the Open Software License version 2.1
+ *  http://opensource.org/licenses/osl-2.1.php
+ */
+package org.dolben.poly;
+
+/**
+ *  A cube.
+ */
+public class Cube extends Cuboid {
+}
