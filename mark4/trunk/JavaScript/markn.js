5b71a6d080563f2ba83a5fbd57b0effa89e611e3
hdolben <hdolben@67ca1e1c-01e3-11df-9f77-c71dee008b03>
2013-02-01 20:29:41 +0000

    Add comments

--- mark4/trunk/JavaScript/markn.js
+++ mark4/trunk/JavaScript/markn.js
@@ -19,11 +19,13 @@
 var DIGITS = 10;
 var PLACES =  4;
 
+// the area where the sequence of guesses and their scores are displayed
 function Scoreboard( id ) {
     this.scoreboard = document.getElementById(id);
     this.clear();
 }
 
+// clears a scoreboard
 Scoreboard.prototype.clear = function( ) {
     while ( this.scoreboard.firstChild ) {
         this.scoreboard.removeChild(this.scoreboard.firstChild);
@@ -34,20 +36,24 @@ Scoreboard.prototype.clear = function( ) {
     this.line = 1;
 }
 
+// scrolls the last line to the bottom of the scoreboard
 Scoreboard.prototype.scroll = function( ) {
     this.scoreboard.scrollTop =
         this.scoreboard.scrollHeight-this.scoreboard.clientHeight;
 }
 
+// puts text (normally a guess) on the next line in the scoreboard
 Scoreboard.prototype.putText = function( text ) {
     this.scoreboard.appendChild(document.createTextNode(text));
     this.scroll();
 }
 
+// puts a guess in the scoreboard
 Scoreboard.prototype.putGuess = function( guess ) {
     this.putText(this.line+": "+guess+" ");
 }
 
+// puts a score in the scoreboard
 Scoreboard.prototype.putScore = function( score ) {
     this.scoreboard.appendChild(document.createTextNode(score));
     this.scoreboard.appendChild(document.createElement("br"));
@@ -55,6 +61,7 @@ Scoreboard.prototype.putScore = function( score ) {
     this.scroll();
 }
 
+// retracts a guess (or message) and the previous score from the scoreboard
 Scoreboard.prototype.retract = function( ) {
     this.line--;
     this.scoreboard.removeChild(this.scoreboard.lastChild);
@@ -63,6 +70,7 @@ Scoreboard.prototype.retract = function( ) {
     this.scroll();
 }
 
+// a permuter of n digits, 0 to n-1
 function RandomDeal( n ) {
     this.digit = new Array(n);
     this.n = n;
@@ -71,6 +79,7 @@ function RandomDeal( n ) {
     }
 }
 
+// returns the next digit in a random sequence
 RandomDeal.prototype.next = function( ) {
     var pick = Math.floor(this.n*Math.random());
     var digit = this.digit[pick];
@@ -81,6 +90,7 @@ RandomDeal.prototype.next = function( ) {
     return digit;
 }
 
+// returns an array of a permuted sequence
 RandomDeal.prototype.getMap = function( ) {
     var shuffle = new Array(this.n);
     for ( var i = 0; i < shuffle.length; i++ ) {
@@ -89,6 +99,7 @@ RandomDeal.prototype.getMap = function( ) {
     return shuffle;
 }
 
+// an automatic number guesser
 function Guesser( ) {
     this.generator = new MonitorsGenerator(DIGITS,PLACES);
     var deal = new RandomDeal(DIGITS);
@@ -99,6 +110,7 @@ function Guesser( ) {
     machine.clear();
 }
 
+// scramble a guess
 Guesser.prototype.mapGuess = function( guess ) {
     var number = new Number();
     
@@ -109,6 +121,8 @@ Guesser.prototype.mapGuess = function( guess ) {
     return number;
 }
 
+// returns the next generated guess and puts in the scoreboard
+// 0 length array if a guess cannot be generated
 Guesser.prototype.guess = function( ) {
     var guess = new Number();
     this.generator.nextGuess(guess);
@@ -124,13 +138,15 @@ Guesser.prototype.guess = function( ) {
     return guess;
 }
 
+// retract the previous score
 Guesser.prototype.retract = function( ) {
     this.generator.retractScore();
     this.inconsistentScores = false;
 }
 
-var guesser;
+var guesser; // the automatic number guesser
 
+// generates guess and scores them until the target is matched
 function autoscore( target ) {
     do {
         var score = target.score(guesser.guess());
@@ -141,6 +157,7 @@ function autoscore( target ) {
     document.getElementById("score").disabled = true;
 }
 
+// start guessing a new target that the user will score
 function retarget( ) {
     guesser = new Guesser();
     guesser.guess();
@@ -148,16 +165,19 @@ function retarget( ) {
     validateScore();
 }
 
+// generates guesses and scores them for a user supplied target
 function target( ) {
     guesser = new Guesser();
     autoscore(getNumber("target"));
 }
 
+// returns that a number supplied the user is valid; has no repeated digits
 function validateNumber( which ) {
     var number = getNumber(which);
     document.getElementById(which).disabled = !number.valid();
 }
 
+// randomly sets the digits of this number
 Number.prototype.random = function( ) {
     var deal = new RandomDeal(DIGITS);
     for ( var place = 0; place < PLACES; place++ ) {
@@ -165,6 +185,7 @@ Number.prototype.random = function( ) {
     }
 }
 
+// randomly sets a target and the automatically guesses it
 function random( ) {
     var number = new Number();
     number.random();
@@ -173,6 +194,7 @@ function random( ) {
     target();
 }
 
+// returns the score set by the user
 function getScore( ) {
     var score = new Score();
     score.setPlaced(document.getElementById("placed").value[0]-'0');
@@ -180,12 +202,15 @@ function getScore( ) {
     return score;
 }
 
+// enables or disables the Score button depending on the validity of the score
+// set by the user
 function validateScore( ) {
     var score = getScore();
     document.getElementById("score").disabled =
         guesser.inconsistentScores || !score.valid(PLACES);
 }
 
+// tells the guess generator the score of its last guess and generates the next
 function score( ) {
     var score = getScore();
     machine.putScore(score);
@@ -199,6 +224,7 @@ function score( ) {
     }
 }
 
+// retracts the last guess and score
 function retract( ) {
     machine.retract();
     guesser.retract();
@@ -206,13 +232,15 @@ function retract( ) {
     validateScore();
 }
 
-var secret = new Number();
+var secret = new Number(); // the target that the user is guessing
 
+// clears the user's guesses and randomly sets a new target for the user
 function newSecret( ) {
     human.clear();
     secret.random();
 }
 
+// returns a number set by the user
 function getNumber( select ) {
     var number = new Number();
     for ( var place = 0; place < PLACES; place++ ) {
@@ -222,6 +250,7 @@ function getNumber( select ) {
     return number;
 }
 
+// puts a number in the user's interface
 Number.prototype.putNumber = function( select ) {
     for ( var place = 0; place < PLACES; place++ ) {
         var s = document.getElementById(select+place);
@@ -229,12 +258,14 @@ Number.prototype.putNumber = function( select ) {
     }
 }
 
+// puts the user's guess and its score into the scoreboard
 function guess( ) {
     var number = getNumber("guess");
     human.putGuess(number);
     human.putScore(secret.score(number));
 }
 
+// puts the user's target into the 'Guess' interface
 function reveal( ) {
     secret.putNumber("guess");
     document.getElementById("guess").disabled = false;
